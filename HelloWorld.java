5e3c586832257418c57b3ce4588914a3055cd6e3
Srinivas2202 <159613548+Srinivas2202@users.noreply.github.com>
2024-02-10 15:46:38 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public static HelloWorld
 	public static void main(string args[])
 	{
 		System.out.println ("Hello World! Welcome to Devops!!!");
-		int a =5
+		int a = 5
 		int b = 10
 		int c = b/a
 		system.output.println ("Divide of a and b =" c)
